e03392612e7e595c679138e175a1834ab9ab9307
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-01-29 20:03:47 +0100

    Use csv instead of xml data to speed up init

--- l10n_ch_bank/__init__.py
+++ l10n_ch_bank/__init__.py
@@ -18,3 +18,12 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 from . import models
+
+
+def post_init(cr, registry):
+    """Import CSV data as it is faster than xml and because we can't use
+    noupdate anymore with csv"""
+    from openerp.tools import convert_file
+    filename = 'data/res.bank.csv'
+    convert_file(cr, 'l10n_ch_bank', filename, None, mode='init',
+                 noupdate=True, kind='init', report=None)
